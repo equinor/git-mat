c3b05d77bdc84bf33853b8a1ccfaa73913498959
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-09-20 23:14:48 +0200

    Refact cloneorpull (#1)
    * feat: try to pull with ff-only first.

--- +GIT/cloneorpullrepo.m
+++ +GIT/cloneorpullrepo.m
@@ -6,7 +6,7 @@ function cloneorpullrepo(folPath,repo,update,branch,verbose)
 %  - folPath - Path to folder
 %
 % OPTIONAL INPUT:
-%  - update  - Set true to pull
+%  - update  - Set true to pull if already cloned.
 %  - repo    - URL to repo.
 %              Defaults to sprintf('https://github.com/Equinor/%s.git',folPath)
 %  - branch  - Defaults to current branch.
@@ -54,6 +54,10 @@ elseif update
         return
     end
 
+    if pull(true)
+        return
+    end
+
     [ahead,behind] = GIT.compareRemote(folPath,currBranch);
     if ahead < 0 && behind < 0
         warning('cloneorpullrepo:localBranch','Repo %s, branch %s is only local. Not possible to update\n',folPath,branch);
@@ -66,21 +70,66 @@ elseif update
     end
 
     if GIT.isdirty(folPath)
-        warning('cloneorpullrepo:dirtyTree','Repo %s is not updated because tree is dirty',folPath);
+        % todo: pull(true) could return output which actually knows what
+        % files would be overwritten
+        warning('cloneorpullrepo:dirtyTree','Repo %s is not updated because tree is dirty and local files would be overwritten by merge',folPath);
         return
     end
 
     if behind > 0
+        pull(false);
+    else
+        if verbose
+            fprintf(1,'Repo %s, branch %s is already up to date\n',folPath,branch);
+        end
+    end
+end
+
+    function TF = pull(ff)
         currDir = pwd;
         c = onCleanup(@()cd(currDir));
         cd(folPath);
         if verbose
-            fprintf(1,'Pulls branch %s from %s\n',branch,folPath);
+            if ff
+                fprintf(1,'Attempts to fast-forward branch %s from %s\n',branch,folPath);
+            else
+                fprintf(1,'Pulls branch %s from %s\n',branch,folPath);
+            end
         end
-        git('pull');
-    else
-        if verbose
-            fprintf(1,'Repo %s, branch %s is already up to date\n',folPath,branch);
+
+        TF = false;
+        if ff
+            [s,out] = git('pull --ff-only');
+        else
+            [s,out] = git('pull');
+        end
+        % if fast forward fails, s will not be 0
+        if isa(s,'double') && s == 0
+            if contains(out,"Already up to date")
+                if verbose
+                    fprintf(1,'Repo %s, branch %s is already up to date\n',folPath,branch);
+                end
+                TF = true;
+            elseif contains(out,sprintf('Fast-forward%s',newline))
+                TF = true;
+            else
+                % disp(out)
+            end
+        else
+            % Check if branch exists, typically master has become main
+            remBranches = GIT.getRemoteBranches();
+            if ~contains(remBranches,sprintf('origin/%s',GIT.getCurrBranch()))
+                if strcmp(GIT.getCurrBranch(),'master')
+                    if contains(remBranches,'origin/main')
+                        if verbose
+                            fprintf(1,'Branch master has been renamed to main in remote. Renames local branch.')
+                        end
+                        git('branch -m main');
+                        git('branch main --set-upstream-to origin/main');
+                        TF = pull(ff);
+                    end
+                end
+            end
         end
     end
 end
